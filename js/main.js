a5a7c0fcb17bc9f74630825a66027100035df667
alex <realcrazyn@yandex.ru>
2021-01-20 17:52:10 +0300

    Топ слайдер и меню

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    $('.banner-section__slider').slick({
+        prevArrow:  '<button class="banner-section__slider-btn banner-section__slider-btnPrev"><img src="/images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="/images/arrow-right.svg" alt=""></button>',
+        dots: true
+    });
 });
