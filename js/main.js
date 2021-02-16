03852068dd754ff799f7115f609d035078d8e95f
alex <realcrazyn@yandex.ru>
2021-02-16 18:49:18 +0300

    page 2 ready

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ $(function(){
 
 	$('.filter-style').styler();
 
-    $('.filter__item-drop').on('click', function(){
+    $('.filter__item-drop, .filter__extra').on('click', function(){
         $(this).toggleClass('filter__item-drop--active');
         $(this).next().slideToggle('200');
     });
@@ -42,10 +42,22 @@ $(function(){
         type: "double",
         min: 100000,
         max: 500000,
-        hide_min_max: false,    // show/hide MIN and MAX labels
-        hide_from_to: false, 
-        force_edges: false, 
+        max_prefix: '5',
     });
     
+    $('.filter-btn__send').on('click', function(){
+        $(this).toggleClass('filter-btn__send--active');
+    });
+
+    $('.catalog__filter-btngrid').on('click', function(){
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btnline').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').removeClass('product-item__wrapper--list');
+    });
+    $('.catalog__filter-btnline').on('click', function(){
+        $(this).addClass('catalog__filter-button--active');
+        $('.catalog__filter-btngrid').removeClass('catalog__filter-button--active');
+        $('.product-item__wrapper').addClass('product-item__wrapper--list');
+    });
 
 });
