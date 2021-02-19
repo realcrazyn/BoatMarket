6e41187a875d570878f8a204cb7f57f338e2b526
alex <realcrazyn@yandex.ru>
2021-02-19 21:54:32 +0300

    page 3 ready

--- js/main.js
+++ js/main.js
@@ -60,4 +60,12 @@ $(function(){
         $('.product-item__wrapper').addClass('product-item__wrapper--list');
     });
 
+    $(".rate-yo").rateYo({
+            ratedFill: "#1C62CD",
+            spacing: "7px",
+            normalFill: "#C4C4C4",
+            starWidth: "23px",
+            starHeight: "23px"
+      });
+
 });
